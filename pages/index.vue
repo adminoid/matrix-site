18ddef5c0bca4ecc655b46b721ea1ea6f992eb03
Petr Zenin <mr@adminoid.com>
2023-09-30 13:43:48 +0300

    font-family default

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,14 @@
 .container
   .row
     .col 1
-    .col 2
-    .col 3
+    .col
+      button.btn.btn-cpink.bg-gradient(type="button") Enter App
+    .col Enter App
 </template>
+
+<style lang="sass" scoped>
+.btn
+  font-family: Inter
+  font-weight: 400
+  font-size: 26px
+</style>
