47e2495de84c604f0e4254b0e77a6166d5a8cd13
Petr Zenin <mr@adminoid.com>
2024-06-04 17:03:46 +0300

    created template for external sass files for components

--- components/pages-components/main/MainIntro.vue
+++ components/pages-components/main/MainIntro.vue
@@ -1,14 +1,9 @@
 <template lang="pug">
-.main-intro
-  h5.main-intro__text International decentralized autonomous platform of mutual assistance and help to pets and wild animals on the BSC blockchain
-  img.main-intro__pigeon.img-fluid.mb-5(src="~/assets/img/pigeon.png")
-  p.main-intro__text Community members connect one after another through a smart contract to endless global lines and automatically receive rewards
+.main-intro.row.row-cols-1.row-cols-md-2
+  .col
+    h5.main-intro__header.col International decentralized autonomous platform of mutual assistance and help to pets and wild animals on the BSC blockchain
+    img.main-intro__pigeon.img-fluid.mb-5.col.d-md-none(src="~/assets/img/pigeon.png")
+    p.main-intro__text.col Community members connect one after another through a smart contract to endless global lines and automatically receive rewards
+  .col.d-md-block.d-none
+    img.main-intro__pigeon.img-fluid.mb-5.col.d-md-block(src="~/assets/img/pigeon.png")
 </template>
-
-<style lang="sass">
-.main-intro
-  &__text
-    text-align: center
-    font-weight: 400
-    line-height: 30px
-</style>
