706d35805504733d8a1d40e3a4a7655e1578e81a
Petr Zenin <mr@adminoid.com>
2024-06-05 15:31:55 +0300

    amount-blocks responsivity

--- components/pages-components/main/AmountBlocks.vue
+++ components/pages-components/main/AmountBlocks.vue
@@ -1,19 +1,23 @@
 <template lang="pug">
 .general-stats
-  main-banner(
-    header="Amount of BNB received from id1 to help animals"
-    link=true
-    footer="0.034"
-  )
-  main-banner(
-    header="Amount of BNB received from donation to help animals"
-    link=true
-    footer="0.034"
-  )
-  main-banner(
-    header="Amount of BNB sent to help animals"
-    :columns="[{ amount: 0.034, title: 'From a wallet id1'}, { amount: 0.034, title: 'From a wallet for donation'}]"
-  )
+  .row.row-cols-1.row-cols-md-3
+    .col
+      main-banner(
+        header="Amount of BNB received from id1 to help animals"
+        link=true
+        footer="0.034"
+      )
+    .col
+      main-banner(
+        header="Amount of BNB received from donation to help animals"
+        link=true
+        footer="0.034"
+      )
+    .col
+      main-banner(
+        header="Amount of BNB sent to help animals"
+        :columns="[{ amount: 0.034, title: 'From a wallet id1'}, { amount: 0.034, title: 'From a wallet for donation'}]"
+      )
 </template>
 
 <script setup>
