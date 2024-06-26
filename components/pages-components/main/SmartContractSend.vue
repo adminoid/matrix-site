0767adbbc0e6df135e39b9c6296437c349f8ba2f
Petr Zenin <mr@adminoid.com>
2024-06-26 15:21:54 +0300

    moved ts code from bnb-web

--- components/pages-components/main/SmartContractSend.vue
+++ components/pages-components/main/SmartContractSend.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.personal-account.mt-5.mt-md-0
+.personal-account
   form
     .personal-account__frame
       .personal-account__form
@@ -9,15 +9,72 @@
             contract-address
             .personal-account__contract-copy
         .personal-account__amount.pb-3
-          label.form-label(for="amount") Send amount
-          input.form-control(name="amount" id="amount")
+          label.form-label(for="send-amount") Send amount
+          input.form-control(
+            type='text'
+            :class="{'is-invalid': !!error}"
+            v-model="amountValue"
+            :disabled="disabled.status"
+            name="send-amount"
+            id="send-amount"
+            required
+          )
+          .invalid-feedback {{ error }}
         .row
           .col
-            button.btn.btn-outline-light.personal-account__button.w-100 Contribution
+            button.btn.btn-outline-light.personal-account__button.w-100(
+              type="button"
+              class="btn btn-outline-primary"
+              @click="sendAmount"
+              :disabled="disabled.status"
+            ) Contribution
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import { ref, watch } from 'vue'
+import { useNuxtApp } from '#app'
+import { useDisabled } from '~/composables/useDisabled'
 import ContractAddress from '~/components/ContractAddress.vue'
+
+const disabled = useDisabled()
+const { $Blockchain } = useNuxtApp()
+
+console.log(disabled)
+
+const amountValue = ref('')
+const error = ref('')
+watch(amountValue, async (newValue) => {
+  await validateValue(newValue)
+})
+
+const validateValue = async (value: any) => {
+  // @ts-ignore
+  const accounts = await $Blockchain.Web3.eth.getAccounts();
+  // @ts-ignore
+  if (!accounts || !$Blockchain.Wallet) {
+    error.value = 'Please connect your wallet first'
+  } else {
+    if (String(value).includes(',')) {
+      value = String(value).replace(/,/g, '.')
+      amountValue.value = value
+    }
+    if (isNaN(Number(value))) {
+      error.value = 'please enter a valid number'
+    } else if (Number(value) <= 0) {
+      error.value = 'please enter a number > 0'
+    } else {
+      error.value = ''
+    }
+  }
+}
+
+const sendAmount = async () => {
+  await validateValue(amountValue.value)
+  if (!error.value) {
+    // @ts-ignore
+    await $Blockchain.sendAmount(String(amountValue.value))
+  }
+}
 </script>
 
 <style lang="sass">
--- pages/main.vue
+++ pages/main.vue
@@ -32,19 +32,20 @@ const {
 const route = useRoute()
 const W = route.params.w
 
-// onMounted(() => {
-//   console.log('ddd', $B.Web3)
-//   console.log('www', W)
-//   console.log('www', $B.Web3.utils.isAddress(W))
-// })
+onMounted(async () => {
+  // console.log('ddd', $B.Web3)
+  // console.log('www', W)
+  // console.log('www', $B.Web3.utils.isAddress(W))
+
+  if ('Web3' in $B && $B.Web3.utils.isAddress(W)) {
+    localStorage.setItem('whose_param', W)
+  }
+  if (route.name !== 'read') {
+    await navigateTo({ path: '/' })
+  }
+
+})
 
 // TODO: set storage by useStorage for wallet (rename) and whose
-
-if ($B.Web3.utils.isAddress(W)) {
-  localStorage.setItem('whose_param', W)
-}
-if (route.name !== 'read') {
-  await navigateTo({ path: '/' })
-}
 
 </script>
