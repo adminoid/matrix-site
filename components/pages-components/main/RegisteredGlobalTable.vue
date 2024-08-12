bb95511f3ac2f77f3cc1b8b33a91d630ff93e088
Petr Zenin <mr@adminoid.com>
2024-08-12 13:53:37 +0300

    global levels filling stable is done

--- components/pages-components/main/RegisteredGlobalTable.vue
+++ components/pages-components/main/RegisteredGlobalTable.vue
@@ -5,29 +5,50 @@
     tbody.table-spec__tbody.table-spec__tbody_strip
       tr
         th(scope="col") Lines1
-        th(v-for="i in 10" :key="i" scope="col") {{ i }}
+        th(
+          v-for="i in 10"
+          :key="i"
+          scope="col"
+        ) {{ i }}
       tr
         td Accounts1
         td(v-for="k in 10")
-          .pink-num {{ k }}
+          .pink-num {{ levels[k - 1] }}
       tr
         th(scope="col") Lines2
-        th(v-for="i in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]" :key="i" scope="col") {{ i }}
+        th(
+          v-for="i in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]"
+          :key="i"
+          scope="col"
+        ) {{ i }}
       tr
         td Accounts2
-        td(v-for="k in 10")
-          .pink-num {{ k }}
+        td(v-for="k in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]")
+          .pink-num {{ levels[k - 1] }}
 </template>
 
 <script setup>
-// const web3Store = useWeb3Store()
-// onMounted(async () => {
-//   console.info('bef cycle')
-//   for (const index in [...Array(20).keys()]) {
-//     const total = await web3Store.getTotalFromMatrix(index)
-//     console.log(index, '). ', Number(total))
-//   }
-// })
+import {useStorage} from "@vueuse/core";
+
+const web3Store = useWeb3Store()
+const isLoaded = ref(false)
+const levels = ref([])
+
+const fillGlobalTable = async () => {
+  for (const index in [...Array(20).keys()]) {
+    const total = await web3Store.getTotalFromMatrix(index)
+    levels.value.push(Number(total))
+    isLoaded.value = true
+  }
+}
+onMounted(async () => {
+  await fillGlobalTable()
+})
+
+const storage = useStorage('connected-wallet', '')
+watch(storage, async () => {
+  await fillGlobalTable()
+})
 </script>
 
 <style lang="sass">
