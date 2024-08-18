f319b5220868718160d27d3950b2592344b51a56
Petr Zenin <mr@adminoid.com>
2024-08-18 10:29:04 +0300

    sumBelowTwoAmount getting fine

--- libs/blockchain/classes.ts
+++ libs/blockchain/classes.ts
@@ -326,61 +326,70 @@ export class External extends Network implements IExternal {
    */
   async getIncomesForId (wallet: string) {
 
+    console.warn('belowTwoEvents', wallet)
+
     const belowTwoEvents = await this.Core.getPastEvents('BelowTwoAppear', {
       filter: {
         receiver: wallet,
       },
-      fromBlock: 'latest',
-      toBlock: 'latest',
-    })
-
-    const claimsAppearEvents = await this.Core.getPastEvents('ClaimsAppear', {
-      filter: {
-        owner: wallet,
-      },
       fromBlock: 0,
       toBlock: 'latest',
     })
 
-    const claimsReferralEvents = await this.Core.getPastEvents('ReferralEarn', {
-      filter: {
-        user: wallet,
-      },
-      fromBlock: 0,
-      toBlock: 'latest',
-    })
+    // const claimsAppearEvents = await this.Core.getPastEvents('ClaimsAppear', {
+    //   filter: {
+    //     owner: wallet,
+    //   },
+    //   fromBlock: 0,
+    //   toBlock: 'latest',
+    // })
+    //
+    // const claimsReferralEvents = await this.Core.getPastEvents('ReferralEarn', {
+    //   filter: {
+    //     user: wallet,
+    //   },
+    //   fromBlock: 0,
+    //   toBlock: 'latest',
+    // })
 
     // todo: sum up belowTwoEvents + claimsAppearEvents amounts
-    console.warn('belowTwoEvents + claimsAppearEvents + claimsReferralEvents')
     console.log('belowTwoEvents:', belowTwoEvents) // todo: sum all
-    console.log('claimsAppearEvents:', claimsAppearEvents) // todo: rewrite next by previous
-    console.log('claimsReferralEvents:', claimsReferralEvents) // todo: rewrite next by previous
+    // console.log('claimsAppearEvents:', claimsAppearEvents) // todo: rewrite next by previous
+    // console.log('claimsReferralEvents:', claimsReferralEvents) // todo: rewrite next by previous
 
+    const initialValue = 0n
     let sumBelowTwoAmount = (belowTwoEvents.length > 0)
         ? belowTwoEvents.reduce((accumulator: any, current: any) => {
-          console.info('sumBelowTwoAmount')
-          console.info(current.returnValues.amount)
-          return current.returnValues.amount + accumulator
-        }) : 0
 
-    let sumClaimsAppearAmount = (claimsAppearEvents.length > 0)
-        ? claimsAppearEvents.reduce((_: any, current: any) => {
-      console.info('claimsAppearEvents')
-      console.info(current.returnValues.newValue)
-      return current.returnValues.newValue;
-    }) : false
+          // console.info('sumBelowTwoAmount')
+          // console.info(current.returnValues.amount)
+          // console.info('matrixIndex;', current.returnValues.matrixIndex)
+          console.log('accumulator..', typeof accumulator, accumulator)
 
-    // todo: get last element
-    let sumClaimsReferralAmount = (claimsReferralEvents.length > 0)
-        ? claimsReferralEvents.reduce((accumulator: any, current: any) => {
-      console.info('claimsReferralEvents')
-      console.info(current.returnValues.amount)
-      return current.returnValues.amount
-    }) : false
+          console.log(typeof current.returnValues.amount, current.returnValues.amount)
+          return BigInt(accumulator) + current?.returnValues?.amount
+        }, initialValue) : 0n
+
+    console.info('sumBelowTwoAmount', Number(sumBelowTwoAmount) / 10**18)
+
+    // let sumClaimsAppearAmount = (claimsAppearEvents.length > 0)
+    //     ? claimsAppearEvents.reduce((_: any, current: any) => {
+    //   console.info('claimsAppearEvents')
+    //   console.info(current.returnValues.newValue)
+    //   return current.returnValues.newValue;
+    // }) : false
+    //
+    // // todo: get last element
+    // let sumClaimsReferralAmount = (claimsReferralEvents.length > 0)
+    //     ? claimsReferralEvents.reduce((accumulator: any, current: any) => {
+    //   console.info('claimsReferralEvents')
+    //   console.info(current.returnValues.amount)
+    //   return current.returnValues.amount
+    // }) : false
 
     console.log(sumBelowTwoAmount)
-    console.log(sumClaimsAppearAmount)
-    console.log(sumClaimsReferralAmount)
+    // console.log(sumClaimsAppearAmount)
+    // console.log(sumClaimsReferralAmount)
 
     return 0
   }
