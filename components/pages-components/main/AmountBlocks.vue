308f14a1c4a3bf92d4245eb6537f46e59a5f87dc
Petr Zenin <mr@adminoid.com>
2024-09-17 23:35:59 +0300

    isShowed fix

--- components/pages-components/main/AmountBlocks.vue
+++ components/pages-components/main/AmountBlocks.vue
@@ -8,13 +8,13 @@
         header="Amount of BNB received from donation to help animals"
         link=true
         footer="0.000"
-        isShowed="true"
+        :isShowed="true"
       )
     .col
       main-banner(
         header="Amount of BNB sent to help animals"
         :columns="[{ amount: '0.000', title: 'From a wallet id1'}, { amount: '0.000', title: 'From a wallet for donation'}]"
-        isShowed="true"
+        :isShowed="true"
       )
 </template>
 
