bb7ca4be12ff2196c1bc06b07e30a18aef2b8ae6
Petr Zenin <mr@adminoid.com>
2024-10-05 19:19:15 +0300

    getting BelowTwoAppear by new way

--- components/pages-components/main/MyRewardsBelowTwo.vue
+++ components/pages-components/main/MyRewardsBelowTwo.vue
@@ -1,35 +1,40 @@
 <template lang="pug">
-.my-rewards-referrals.row.mb-2
-  .col-10 Amount of BNB received to the wallet
-  .col-2.text-end(v-if="isLoaded") {{ totalBnb }}
-  .col-2.text-end(v-else) <i>Loading...</i>
+  .my-rewards-referrals.row.mb-2
+    .col-10 Amount of BNB received to the wallet
+    .col-2.text-end(v-if="isLoaded") {{ totalBnb }}
+    .col-2.text-end(v-else) <i>Loading...</i>
 </template>
 
 <script setup>
 import {getBC} from '~/stores/useWeb3.js'
+import {GetEvents} from "~/libs/events-infura/abi-events.js";
+import {isClient} from "@vueuse/core";
 
 const BC = await getBC()
 
 const totalBnb = ref(0)
 const isLoaded = ref(false)
 const fillEvents = async () => {
-  // todo => restore mark
-  // const eventsFound = await BC.value.getBelowTwoAppear()
-  // let amount = 0n
-  // if (eventsFound.length > 0) {
-  //   for (const evt of eventsFound) {
-  //     amount = amount + evt?.returnValues.amount
-  //   }
-  // } else {
-  //   amount = 0
-  // }
-  //
-  // totalBnb.value = Number(amount) / 10**18
+  if (!isClient) return
+
+  const eventsFound = await GetEvents('BelowTwoAppear')
+  let amount = 0n
+  if (eventsFound.length > 0) {
+    for (const evt of eventsFound) {
+      amount = amount + evt.amount
+    }
+  } else {
+    amount = 0
+  }
+
+  totalBnb.value = Number(amount) / 10**18
   isLoaded.value = true
 }
 
-onMounted(async () => {
-  await fillEvents()
+watch(isInitialized, (nv) => {
+  if (nv) {
+    fillEvents()
+  }
 })
 
 useNuxtApp().$on('wallet-updated', async () => {
