e2dfdbaff5d716d926a38090a1e2c83baf986398
Petr Zenin <mr@adminoid.com>
2025-11-21 01:00:24 +0300

    made mini version of pigeon png's

--- components/pages-components/main/PhilanthropistBanner.vue
+++ components/pages-components/main/PhilanthropistBanner.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .philanthropist-banner
   .row
-    img.philanthropist-banner__image.img-fluid.mx-md-auto(src="~/assets/img/violet-pigeon.png")
+    img.philanthropist-banner__image.img-fluid.mx-md-auto(src="~/assets/img/violet-pigeon.jpg")
   h5.centered-header Feel like a cryptophilanthrope
   .philanthropist-banner__text Participation in the project will not only allow you to receive rewards, but also give you the opportunity to extend mercy by helping community members and their pets in hardships and help wild animals around the World
   a.philanthropist-banner__email(href="mailto:help@givedream.io") help@givedream.io
